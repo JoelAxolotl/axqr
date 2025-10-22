38038dd345b972f6a73d76c8cbbf4be96f313d8b
Joel-PC <joeldevstudio@outlook.com>
2025-10-22 18:34:44 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,86 @@
-import Image from "next/image";
+"use client";
+
+import qrcode from "qrcode-generator";
+import { useRef, useState } from "react";
+import { FaConnectdevelop } from "react-icons/fa";
+import { IoQrCode } from "react-icons/io5";
+import { MdTypeSpecimen } from "react-icons/md";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	const divRef = useRef<HTMLDivElement>({} as HTMLDivElement);
+	const [prompt, setPrompt] = useState<string>("");
+	const [dataUrl, setDataUrl] = useState<string>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	function generate() {
+		if (prompt == "") { return; };
+
+		var qr = qrcode(0, "L");
+		qr.addData(prompt);
+		qr.make();
+
+		setDataUrl(qr.createDataURL(64));
+	}
+
+	return (
+		<main className="h-screen">
+			<section className="h-screen flex flex-col">
+				<div className="container mx-auto flex flex-col items-center px-10 py-5">
+					<h1 className="font-bold text-4xl">AxQR</h1>
+					<p className="text-center">Generate your own QR Code for free!</p>
+				</div>
+
+				<div className="container mx-auto flex flex-col items-center text-black">
+					<div className="bg-white lg:w-[500px] sm:w-[400px] h-[500px] px-5 py-5 rounded-xl
+					flex flex-col items-stretch justify-stretch">
+
+						<div className="h-full flex flex-col items-center">
+							<label className="w-full">Type Here:</label>
+							<div className="w-full relative">
+								<MdTypeSpecimen className="absolute left-[10px] top-[17px]" fontSize={18} />
+								<input type="text" className="border border-[#00000046] hover:border-black duration-500
+						outline-none px-2 py-1 rounded-xl w-[100%] mt-[10px] pl-[36px]"
+									onChange={(e) => { setPrompt(e.target.value); }} placeholder="Your text here..." />
+							</div>
+
+							<br />
+
+							{
+								dataUrl == "" && (
+									<div className="w-[300px] h-[300px] flex flex-col items-center justify-center">
+										<p>
+											Type your text then click generate!
+										</p>
+									</div>
+								)
+							}
+							{
+								dataUrl && (
+									<img className="w-[300px] h-[300px]" src={dataUrl} />
+								)
+							}
+
+						</div>
+
+						<br />
+
+						<button className="bg-green-300 py-[5px] rounded-xl
+						flex items-center justify-center gap-2
+						hover:bg-green-400 hover:rounded-[0px] duration-300"
+							onClick={generate}>
+							<IoQrCode fontSize={18} />
+							Generate
+						</button>
+					</div>
+				</div>
+
+				<div className="mt-auto mb-[32px] flex items-center justify-center gap-2">
+					<FaConnectdevelop fontSize={18} />
+					<p>
+						Made By Joel
+					</p>
+				</div>
+
+			</section>
+		</main>
+	);
 }
